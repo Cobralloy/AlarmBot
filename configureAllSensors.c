d650bde6f913e9545386f8e7e821bb3dd07b0584
Cobralloy <57238270+Cobralloy@users.noreply.github.com>
2023-02-16 16:21:22 -0500

    Add files via upload

---
+++ configureAllSensors.c
@@ -0,0 +1,17 @@
+void configureAllSensors()
+{
+	SensorType[S1] = sensorSoundDBA;
+	SensorType[S2] = sensorEV3_Ultrasonic;
+
+	SensorType[S3] = sensorEV3_Color;
+	wait1Msec(50);
+	SensorMode[S3] = modeEV3Color_Color;
+	wait1Msec(50);
+
+	SensorType[S4] = sensorEV3_Gyro;
+	wait1Msec(50);
+	SensorMode[S4] = modeEV3Gyro_Calibration;
+	wait1Msec(100);
+	SensorMode[S4] = modeEV3Gyro_RateAndAngle;
+	wait1Msec(50);
+}
