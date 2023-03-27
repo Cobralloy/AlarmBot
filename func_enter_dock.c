dfbdefc4966de1e383ede6c3ea20ef80120c816c
Cobralloy <57238270+Cobralloy@users.noreply.github.com>
2023-03-27 17:12:40 -0400

    Update and rename func_returnDoc to func_enter_dock.c

---
+++ func_enter_dock.c
@@ -0,0 +1,12 @@
+void enter_dock(int motor_power)
+{
+  	drive(motor_power, -motor_power);
+	while(getGyroDegrees != 0)
+  	{}
+  	drive(0, 0);
+  	wait1Msec(1000);
+  	drive(motor_power/2, motor_power/2);
+  	while(readMuxSensor(msensor_S4_1)) == 0)
+  	{}
+  	driveDistance(-3, -motor_power/2);
+}
