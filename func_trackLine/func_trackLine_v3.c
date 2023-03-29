6b8e2e9db05d9e2bb950ba5bfbe7f5ae2ce4e985
Cobralloy <57238270+Cobralloy@users.noreply.github.com>
2023-03-29 15:22:12 -0400

    Update func_trackLine_v3.c

--- func_trackLine/func_trackLine_v3.c
+++ func_trackLine/func_trackLine_v3.c
@@ -1,19 +1,22 @@
 int detectSurface(long &red, long &green, long &blue)
 {
 	// Target values[][0 to 2] and tolerances[][3 to 5]
-	const long TAPE [4][6] = {{78, 10, 8, 5, 5, 5}, // red tape
+	const long TAPE [5][6] = {{78, 10, 8, 5, 5, 5}, // red tape
 								{6, 9, 6, 5, 5, 5}, // black tape
-								{50, 15, 15, 10, 5, 5}, // center
-								{83, 91, 73, 10, 10, 10}}; // white
+								{40, 10, 8, 50, 5, 5}, // center
+								{83, 91, 73, 15, 15, 15}, // white
+								{32, 57, 10, 25, 10, 15}}; // green
 	// WEEF Lab RGB values: {r, g, b}
 	getColorRGB(S3, red, green, blue);
-	for (int i = 0; i <= 3; i++)
+	for (int i = 0; i <= 4; i++)
 	{
-		if (abs(TAPE[i][0] - red) < TAPE[i][3] 
-			&& abs(TAPE[i][1] - green) < TAPE[i][4] 
-			&& abs(TAPE[i][2] - blue) < TAPE[i][5])
+		//displayBigTextLine(6, "%d %d %d", abs(TAPE[4][0] - red), abs(TAPE[4][1] - green), abs(TAPE[i][2] - blue) < TAPE[4][5]);
+		//displayBigTextLine(8, "%d %d %d", TAPE[4][3], TAPE[4][4], TAPE[4][5]);
+		if (abs(TAPE[i][0] - red) < TAPE[i][3]		// compare red
+			&& abs(TAPE[i][1] - green) < TAPE[i][4] // compare green
+			&& abs(TAPE[i][2] - blue) < TAPE[i][5]) // compare blue
 		{
-			return i+1; // red tape 1, black tape 2, center 3, white 4
+			return i+1; // red tape 1, black tape 2, center 3, white 4, green 5
 		}
 	}
 	return 0; // none 0
