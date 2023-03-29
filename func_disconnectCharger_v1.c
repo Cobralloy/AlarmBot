70ffa1e3c9754ee39d96a50ec27da24f19bcf518
Cobralloy <57238270+Cobralloy@users.noreply.github.com>
2023-03-28 20:29:48 -0400

    Update func_disconnectCharger_v1.c

--- func_disconnectCharger_v1.c
+++ func_disconnectCharger_v1.c
@@ -2,16 +2,16 @@ bool disconnectCharger() //Devaditya Chakrabarty
 {
 	nMotorEncoder[motorB] = 0;
 	motor[motorB] = 100;
-	wait1Msec(1000);
+	wait1Msec(500);
 	drive(-100,-100);
 	clearTimer(T2);
 	while(nMotorEncoder[motorB] < 90 && time1[T2] < 3000)
 	{}
 	drive(0,0);
 	motor[motorB] = 0;
-	if(time1[T2] > 2999)
+	if (time1[T2] > 2999)
 	{
-		driveDistance(10, 100)
+		driveDistance(8, 100)
 		return false;
 	}
 	else
