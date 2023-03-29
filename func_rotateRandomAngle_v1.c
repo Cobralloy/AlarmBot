3bea712aef6e34f3d05e0b5375c19e03a0a3fb0c
Cobralloy <57238270+Cobralloy@users.noreply.github.com>
2023-03-29 14:01:24 -0400

    Update func_rotateRandomAngle_v1.c

--- func_rotateRandomAngle_v1.c
+++ func_rotateRandomAngle_v1.c
@@ -1,9 +1,7 @@
 void rotateRandomAngle (int min_angle, int max_angle, int motorpower)
 {
 	int angle = randomAngle (-180,180);
-	while ( abs(angle) < min_angle && abs(angle) > max_angle)
-	{
+	while (abs(angle) < min_angle && abs(angle) > max_angle)
 		angle = randomAngle (-180,180);
-	}
 	rotate(angle,motorpower);
 }
