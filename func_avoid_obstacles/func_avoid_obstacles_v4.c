0f45f40eb8f37fba091e1c0b9a5e4e67c8ddd369
Cobralloy <57238270+Cobralloy@users.noreply.github.com>
2023-03-29 18:06:35 -0400

    Create func_avoid_obstacles_v4.c

---
+++ func_avoid_obstacles/func_avoid_obstacles_v4.c
@@ -0,0 +1,20 @@
+void avoid_obstacles ()
+{
+	if (SensorValue[S3] == (int) colorRed)
+	{
+		rotateRandomAngle (90,180,50);
+	}
+	else if (readMuxSensor(msensor_S1_1) == 1)
+	{
+		drive(-15, 70);
+		rotateRandomAngle(45, 90, 50);
+	}
+	else if (readMuxSensor(msensor_S1_2) < LENGTH)
+	{
+		rotateRandomAngle (120,180,100);
+	}
+	else if (readMuxSensor(mesensor_S1_2 < LENGTH + 30)
+	{
+		rotateRandomAngle(30, 90, 50);
+	}
+}
