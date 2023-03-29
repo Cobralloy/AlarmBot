4b3b90af198a02af18a573a8515305267ed871ed
Cobralloy <57238270+Cobralloy@users.noreply.github.com>
2023-03-29 09:27:13 -0400

    Update func_avoid_obstacles_v3.c

--- func_avoid_obstacles/func_avoid_obstacles_v3.c
+++ func_avoid_obstacles/func_avoid_obstacles_v3.c
@@ -1,6 +1,5 @@
 void avoid_obstacles ()
 {
-	const float LENGTH = 30;
 	while (readMuxSensor(msensor_S1_2) < LENGTH + 30 || readMuxSensor(msensor_S1_1) == 1 || SensorValue[S3] == (int) colorRed)
 	{
 		if (SensorValue[S3] == (int) colorRed)
