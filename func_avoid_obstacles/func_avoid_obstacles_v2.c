723053087c2e01a7b419d589b15247d62274fcd5
Cobralloy <57238270+Cobralloy@users.noreply.github.com>
2023-03-28 20:22:49 -0400

    holy shit prab you have monkey code

--- func_avoid_obstacles/func_avoid_obstacles_v2.c
+++ func_avoid_obstacles/func_avoid_obstacles_v2.c
@@ -1,20 +1,25 @@
-// Avoid Obstacle Function - Prabhjyot Paul
-const float LENGTH = 35;
 void avoid_obstacles ()
 {
-	while (readMuxSensor(msensor_S4_2)< LENGTH || readMuxSensor(msensor_S4_1) == 1 || SensorValue(S3) == (int)colorRed)
+	// Avoid Obstacle Function - Prabhjyot Paul
+	const float LENGTH = 30;
+	while (readMuxSensor(msensor_S1_2) < LENGTH + 30 || readMuxSensor(msensor_S1_1) == 1 || SensorValue[S3] == (int) colorRed)
 	{
-		if(SensorValue (S3) == (int)colorRed)
+		if (SensorValue[S3] == (int) colorRed)
 		{
-			rotateRandomAngle (90,180,50 );
+			rotateRandomAngle (90,180,50);
 		}
-		else if (SensorValue(sonarCM) < 35)
+		else if (readMuxSensor(msensor_S1_1) == 1)
 		{
-			rotateRandomAngle (10,135,65 );
+			drive(-15, 70);
+			rotateRandomAngle(45, 90, 50);
+		}
+		else if (readMuxSensor(msensor_S1_2) < LENGTH)
+		{
+			rotateRandomAngle (120,180,100);
 		}
 		else
 		{
-			rotateRandomAngle (45,167,75)
+			rotateRandomAngle(30, 90, 50);
 		}
 	}
 }
