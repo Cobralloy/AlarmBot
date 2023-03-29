4f25f7e4317321b5dc13e771be2d81b3d596d8b1
Cobralloy <57238270+Cobralloy@users.noreply.github.com>
2023-03-28 23:50:42 -0400

    Create func_alarmStatus_v3.c

---
+++ func_alarmStatus/func_alarmStatus_v3.c
@@ -0,0 +1,52 @@
+const int NUM_READINGS = 40; // MS_INTERVAL * NUM_READINGS = READING WINDOW
+
+float calculateMean(int *values)
+{
+	float sum = 0;
+	for (int i = 0; i < NUM_READINGS; i++)
+	{
+		sum += values[i];
+	}
+	return sum /= NUM_READINGS;
+}
+
+int returnMax(int *values)
+{
+	int max = 0;
+	for (int i = 0; i < NUM_READINGS; i++)
+	{
+		if (values[i] > max)
+			max = values[i];
+	}
+	return max;
+}
+
+bool alarmStatus(int *readings, int *timestamps, int &index)
+{
+	const float THRESHOLD = 20;
+	const int MS_INTERVAL = 50; // MS_INTERVAL * NUM_READINGS = READING WINDOW
+	if (index == 0)
+	{
+		if (time1[T1] - timestamps[NUM_READINGS-1] > MS_INTERVAL)
+		{
+			timestamps[index] = time1[T1];
+			readings[index] = SensorValue[S4];
+			index++;
+		}
+	}
+	else
+	{
+		if (time1[T1] - timestamps[index-1] > MS_INTERVAL)
+		{
+			timestamps[index] = time1[T1];
+			readings[index] = SensorValue[S4];
+			index++;
+		}
+
+	}
+	if (index > NUM_READINGS)
+		index = 0;
+	if (calculateMean(readings) > THRESHOLD && returnMax(readings) > 40)
+		return true;
+	return false;
+}
