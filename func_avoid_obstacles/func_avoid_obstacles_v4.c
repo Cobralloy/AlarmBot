a2a5b362573c88e8cc6c2a980fc7bfd8441d7bf3
Cobralloy <57238270+Cobralloy@users.noreply.github.com>
2023-03-29 21:08:46 -0400

    Update func_avoid_obstacles_v4.c

--- func_avoid_obstacles/func_avoid_obstacles_v4.c
+++ func_avoid_obstacles/func_avoid_obstacles_v4.c
@@ -1,3 +1,11 @@
+void rotateRandomAngle (int min_angle, int max_angle, int motorpower)
+{
+	int angle = randomAngle (-180,180);
+	while (abs(angle) < min_angle && abs(angle) > max_angle)
+		angle = randomAngle (-180,180);
+	rotateAngle(angle,motorpower);
+}
+
 void avoid_obstacles ()
 {
 	if (SensorValue[S3] == (int) colorRed)
