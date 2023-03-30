9b322e6b8248ee40427fb9ca99d65da0d0a24d98
Cobralloy <57238270+Cobralloy@users.noreply.github.com>
2023-03-29 21:04:54 -0400

    Update func_enter_dock_v3.c

--- func_enter_dock/func_enter_dock_v3.c
+++ func_enter_dock/func_enter_dock_v3.c
@@ -3,14 +3,14 @@ void enter_dock()
 	const int motor_power = 30;
 	if (getGyroDegrees(S2) > 180)
 		drive(motor_power, -motor_power);
-	else
+	else if (getGyroDegrees(S2) < 180)
 		drive(-motor_power, motor_power);
-	while(getGyroDegrees(S2)%360 != 0)
+	while(getGyroDegrees(S2) % 360 != 0)
 	{}
 	drive(0, 0);
 	wait1Msec(1000);
 	drive(motor_power/2, motor_power/2);
 	while(readMuxSensor(msensor_S1_1) == 0)
 	{}
-	driveDistance(-10, -motor_power/2);
+	driveDistance(-5, -motor_power/2);
 }
