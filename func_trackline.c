d40e8885610f2b92fc370a71cdf8cfa5b24fd831
Cobralloy <57238270+Cobralloy@users.noreply.github.com>
2023-04-03 00:09:08 -0400

    Update func_trackline.c

--- func_trackline.c
+++ func_trackline.c
@@ -2,16 +2,13 @@ int detectSurface(long &red, long &green, long &blue)
 {
 	// Target values[][0 to 2] and tolerances[][3 to 5]
 	const long TAPE [5][6] = {{80, 10, 8, 20, 5, 5}, // red tape
-								{8, 9, 6, 5, 5, 5}, // black tape
-								{40, 16, 8, 30, 12, 5}, // center
-								{74, 82, 67, 20, 20, 20}, // white
-								{32, 57, 10, 25, 10, 15}}; // green
-	// WEEF Lab RGB values: {r, g, b}
+				{8, 9, 6, 5, 5, 5}, // black tape
+				{40, 16, 8, 30, 12, 5}, // center
+				{74, 82, 67, 20, 20, 20}, // white
+				{32, 57, 10, 25, 10, 15}}; // green
 	getColorRGB(S3, red, green, blue);
 	for (int i = 0; i <= 4; i++)
 	{
-		//displayBigTextLine(6, "%d %d %d", abs(TAPE[4][0] - red), abs(TAPE[4][1] - green), abs(TAPE[i][2] - blue) < TAPE[4][5]);
-		//displayBigTextLine(8, "%d %d %d", TAPE[4][3], TAPE[4][4], TAPE[4][5]);
 		if (abs(TAPE[i][0] - red) < TAPE[i][3]		// compare red
 			&& abs(TAPE[i][1] - green) < TAPE[i][4] // compare green
 			&& abs(TAPE[i][2] - blue) < TAPE[i][5]) // compare blue
@@ -25,12 +22,13 @@ int detectSurface(long &red, long &green, long &blue)
 void trackLine()
 {
 	// Alan
-	SensorMode[S3] = modeEV3Color_Color;
 	long red = 0, green = 0, blue = 0;
+	
+	// Drive forward until line is detected
 	drive(30, 30);
 	while (detectSurface(red, green, blue) != 3)
 	{}
-	displayBigTextLine(1, "Line detected");
+	
 	int surface = detectSurface(red, green, blue);
 	while (surface != 5)
 	{
@@ -39,7 +37,7 @@ void trackLine()
 		{
 			while (detectSurface(red, green, blue) != 3)
 				drive(15, 3);
-			drive(0, 3);
+			drive(0, 3); // Correct overshoot
 			wait1Msec(30);
 		}
 		else if (surface == 2)
